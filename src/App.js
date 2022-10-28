d329180e0257454f2f9a30ed433a9e50454da115
Shivam Sharma <sharmashushant118@gmail.com>
2022-10-28 22:53:26 +0530

    Table Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BiEdit } from "react-icons/bi";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table cellSpacing={0}>
+        <tr>
+          <td colspan="5">
+            <input
+              type="text"
+              className="searchBar"
+              placeholder="Search by name, email or role"
+            />
+          </td>
+        </tr>
+        <tr>
+          <th>
+            <input type="checkbox" />
+          </th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th>Actions</th>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>Shivam</td>
+          <td>sharma@gmail.com</td>
+          <td>Member</td>
+          <td>
+          <span className="edit">
+            <BiEdit />
+          </span>
+          <span className="delete">
+            <AiOutlineDelete />
+          </span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>Shivam</td>
+          <td>sharma@gmail.com</td>
+          <td>Member</td>
+          <td>
+          <span className="edit">
+            <BiEdit />
+          </span>
+          <span className="delete">
+            <AiOutlineDelete />
+          </span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>Shivam</td>
+          <td>sharma@gmail.com</td>
+          <td>Member</td>
+          <td>
+          <span className="edit">
+            <BiEdit />
+          </span>
+          <span className="delete">
+            <AiOutlineDelete />
+          </span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>Shivam</td>
+          <td>sharma@gmail.com</td>
+          <td>Member</td>
+          <td>
+          <span className="edit">
+            <BiEdit />
+          </span>
+          <span className="delete">
+            <AiOutlineDelete />
+          </span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>Shivam</td>
+          <td>sharma@gmail.com</td>
+          <td>Member</td>
+          <td>
+          <span className="edit">
+            <BiEdit />
+          </span>
+          <span className="delete">
+            <AiOutlineDelete />
+          </span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button className="btnDelete">Delete Selected</button>
+          </td>
+          <td colspan="4" className="btnPages">
+            <button className="btnPage">&lt;&lt;</button>
+            <button className="btnPage">&lt;</button>
+            <button className="btnPage active">1</button>
+            <button className="btnPage">2</button>
+            <button className="btnPage">3</button>
+            <button className="btnPage">&gt;</button>
+            <button className="btnPage">&gt;&gt;</button>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
